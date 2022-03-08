b08eeb02ce0f5d95e785b8bc6d2bb7e6f8bd768d
Alvaro Vaca <alvarovacamendoza@gmail.com>
2022-03-08 14:06:43 -0800

    Finalized Perpetual Motion Machine Files

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ from time import sleep
 import RPi.GPIO as GPIO 
 from pidev.stepper import stepper
 from pidev.Cyprus_Commands import Cyprus_Commands_RPi as cyprus
+from threading import Thread
 
 
 # ////////////////////////////////////////////////////////////////
@@ -44,6 +45,7 @@ TOP = False
 OPEN = False
 CLOSE = True
 YELLOW = .180, 0.188, 0.980, 1
+GREY = 144/255, 136/255, 136/255, 1
 BLUE = 0.917, 0.796, 0.380, 1
 DEBOUNCE = 0.1
 INIT_RAMP_SPEED = 150
@@ -68,7 +70,8 @@ cyprus.open_spi()
 # //                    SLUSH/HARDWARE SETUP                    //
 # ////////////////////////////////////////////////////////////////
 sm = ScreenManager()
-ramp = stepper(port = 0, speed = INIT_RAMP_SPEED)
+s0 = stepper(port=0, micro_steps=32, hold_current=20, run_current=20, accel_current=20, deaccel_current=20,
+             steps_per_unit=200, speed=2)
 
 # ////////////////////////////////////////////////////////////////
 # //                       MAIN FUNCTIONS                       //
@@ -87,39 +90,76 @@ ramp = stepper(port = 0, speed = INIT_RAMP_SPEED)
 class MainScreen(Screen):
     version = cyprus.read_firmware_version()
     staircaseSpeedText = '0'
-    rampSpeed = INIT_RAMP_SPEED
-    staircaseSpeed = 40
+
 
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
         self.initialize()
 
     def toggleGate(self):
-        print("Open and Close gate here")
+        cyprus.set_servo_position(2, 1)
+        sleep(1)
+        cyprus.set_servo_position(2, 0)
+        print("Open gate")
 
     def toggleStaircase(self):
+        cyprus.set_pwm_values(1, period_value=100000, compare_value=10000*self.staircaseSpeed.value, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
+        sleep(5.5)
+        cyprus.set_pwm_values(1, period_value=100000, compare_value=0, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
         print("Turn on and off staircase here")
         
     def toggleRamp(self):
+        s0.start_relative_move(28.5)
+        while s0.is_busy():
+            sleep(1)
+        s0.go_until_press(0, self.rampSpeed.value * 6400)
         print("Move ramp up and down here")
-        
+
+    def threadtoggleRamp(self):
+        Thread(target=self.toggleRamp, daemon=True).start()
+
     def auto(self):
+        self.ramp.disabled = True
+        self.gate.disabled = True
+        self.staircase.disabled = True
+        self.ids.gate.color = GREY
+        self.ids.staircase.color = GREY
+        self.ids.ramp.color = GREY
+        cyprus.set_pwm_values(1, period_value=100000, compare_value=10000*self.staircaseSpeed.value, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
+        sleep(5.6)
+        cyprus.set_pwm_values(1, period_value=100000, compare_value=0, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
+        cyprus.set_servo_position(2, 1)
+        sleep(1)
+        cyprus.set_servo_position(2, 0)
+        sleep(1.1)
+        s0.start_relative_move(28.5)
+        while s0.is_busy():
+            sleep(1)
+        s0.go_until_press(0, self.rampSpeed.value * 6400)
+        sleep(20)
         print("Run through one cycle of the perpetual motion machine")
+
+    def threadauto(self):
+        Thread(target=self.auto, daemon=True).start()
         
-    def setRampSpeed(self, speed):
+    def setrampSpeed(self, speed):
+        s0.set_speed(self.rampSpeed.value)
         print("Set the ramp speed and update slider text")
         
-    def setStaircaseSpeed(self, speed):
+    def setstaircaseSpeed(self, speed):
+        #cyprus.set_pwm_values(1, period_value=100000, compare_value=10000*self.staircaseSpeed.value, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
         print("Set the staircase speed and update slider text")
         
     def initialize(self):
+        cyprus.initialize()
+        cyprus.setup_servo(2)
+        s0.go_until_press(0, 3 * 6400)
         print("Close gate, stop staircase and home ramp here")
 
     def resetColors(self):
-        self.ids.gate.color = YELLOW
-        self.ids.staircase.color = YELLOW
-        self.ids.ramp.color = YELLOW
-        self.ids.auto.color = BLUE
+        self.ids.gate.color = GREY
+        self.ids.staircase.color = GREY
+        self.ids.ramp.color = GREY
     
     def quit(self):
         print("Exit")
